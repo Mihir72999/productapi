5990d6e93df4072d2a5942591bfd6e279500b47e
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-08-04 12:33:17 +0530

    Update route.ts

--- route/route.ts
+++ route/route.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express'
+import * as fs from 'fs'
 
 import { getBrandmodel, getComment, getProduct, postComment, updateComment } from '../controller/controller'
 
@@ -6,7 +7,10 @@ import { getBrandmodel, getComment, getProduct, postComment, updateComment } fro
 const router = Router({ strict: true, caseSensitive: false, mergeParams: true })
 
 router.get('/', (req: Request, res: Response) => {
-   res.send('welcome to productApi')
+   fs.readFile(`build/prisma.html`,'utf-8',(error,resonse)=>{
+   if(error) console.log(error)
+   res.send(resonse)
+  })
 })
 
 
