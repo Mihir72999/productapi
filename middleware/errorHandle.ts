bb5687bf2706983ee2fcd3898f14affe5d0c1fa7
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2024-04-10 22:36:19 -0700

    Update errorHandle.ts

--- middleware/errorHandle.ts
+++ middleware/errorHandle.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 const errorHandle = (error: any, req: Request, res: Response, next: NextFunction) => {
-  const status = res.statusCode ? res.statusCode : 500
+const status = error.statusCode ? error.statusCode : 500
 
 
 
